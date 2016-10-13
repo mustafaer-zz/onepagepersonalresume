5ead7267ed0e283e81c92b4027f9af07493cf948
mustafaer <mustafaer243@gmail.com>
2016-10-14 00:32:33 +0300

    Kişisel tanıtım sayfası

--- index.php
+++ index.php
@@ -62,12 +62,20 @@
        	    <li class="col-lg-2 col-md-2 col-sm-4 col-xs-4"><a href="https://instagram.com/MustafaER53/" target=".blank"><i class="fa fa-instagram fa-fw" aria-hidden="true"></i></a></li>
         </ul></h1><br>
    <div style="margin-top: 35px;" class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-       <img src="img/mustafaer.jpg" class="img-thumbnail" alt="Cinque Terre" width="304" height="236">
+       <img src="img/mustafaer.jpg" class="img-thumbnail" alt="Mustafa ER - Img" width="304" height="236">
    </div>
    <h1 style="border: 2px solid #262626;"></h1>
    <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
        <h2>My Name is Mustafa ER, and I am a Electrical - Electronical Engineering Student</h2>
-       <p>I was born on March 17, 1995 in Rize. Rize passed a long period of my life. The first school Rize / Güneysu Ulucami I read in elementary school. SBS exam at the end of 8 years Güneysu S.K.M the Anatolian Teacher High School (now called the Güneysu S.K.M Science High School), I settled. I spent 4 years there. Anatolian Teacher High School as part due to my not reading electronic despite interest from primary college choice on my list had only the Department of Electrical and Electronic Engineering. So I would not be surprised my goal. Currently in Electrical and Electronics Engineering Department at Sakarya University I continue my education. Although my first year at the school process in order to improve myself, I decided to enroll in a variety of clubs and I made reference to follow clubs associated with the department. Currently, I thought it would be the most useful of these clubs "<b><a href="https://www.facebook.com/sauieee" style="text-decoration: none;">IEEE SAU Student Branch</a></b>" A "<b><a href="https://www.facebook.com/ieeesaucomsoc" style="text-decoration: none;">Chairman Of Communications Society</a></b>" (formerly "Electronical Electrical Team Leader and Project Developer") as I receive the task. For contact "mail@mustafaer.pro" my e-mail address or use the contact section. You can examine the <a href="https://tr.linkedin.com/in/mustafaer/">LinkedIn</a> network for professional profiles.</p> 
+       <p>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Rize de doğdum. İlk okulu Güneysu Ulucami İlköğretim okulunda okudum. Liseyi ise Güneysu Anadolu Öğretmen Lisesinde tamamladım. Şu anda Sakarya Üniversitesi Elektrik Elektronik Mühendisli Bölümünde eğitimime devam etmekteyim.
+	<br>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  Okulda aldığım eğitim haricinde HTML, CSS, PHP, ASP.NET web tasarımı ve programlama dil ve teknolojilerinde kendimi geliştirdim. Ayrıca C# ve DELPHİ ile masaüstü programlama bilgisine sahibim. Arduino, CCS C, Energia, 8051 Assembly ile donanımsal programlama bilgisine sahibim.Elektronik devre konusunda PROTEUS ile tasarım, basalabilir hale getirme konusunda bilgi sahibiyim. Ayrıca prototip oluştumak için baskı ve dizgi bilgisine sahibim.
+        <br>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>"<a href="https://facebook.com/sauieee" style="text-decoration: none;">IEEE SAU Student Branch</a>"</b> ailesinde <b>"<a href="https://facebook.com/sauieeecomsoc" style="text-decoration: none;">Chairman Of Communications Society</a>"</b> görevini üstlenmekteyim.
+        <br>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Profesyonel profilim için <a href="https://www.linkedin.com/in/mustafaer" style="text-decoration: none;">Linkedln</a> ağımı inceleyebilir, <a href="http://cv.mustafaer.pro" style="text-decoration: none;">CV</a> mi inceleyerek yeteneklerim hakkında daha fazla bilgi sahibi olabilir , <a href="http://portfolyo.mustafaer.pro" style="text-decoration: none;">PORTFOLYO</a> mu incelerek yapmış olduğum çalışmalara ulaşabilirsiniz.
+       </p>
    </div>
    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
    <h1 style="border: 2px solid #262626;"></h1>
